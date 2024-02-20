47f443607f09d462759f9bd6cc3b5345e2afd220
ShravaniR2412 <shravanirasam0212@gmail.com>
2024-02-20 11:57:02 +0530

    routes added

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { BrowserRouter , Router, Route,Routes } from "react-router-dom";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Pages/Login'
-import SignUp from './Pages/Signup'
+import Signup from './Pages/Signup'
+import Blogs from './Pages/Blogs'
+import Readblog from './Pages/Readblog'
+import AddBlog from './Pages/Addblog'
+
 
 
 function App() {
@@ -11,7 +17,15 @@ function App() {
 
   return (
     <>
-    <SignUp/>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/addblog" element={<AddBlog />} />
+          <Route path="/readblog/:id" element={<Readblog />} />
+        </Routes>
+    </BrowserRouter>
     
     </>
   )
